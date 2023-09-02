71e8e8a1d84ff2c8659496496223ecc085b73524
rejkid <rejkid@gmail.com>
2023-09-02 17:07:04 +1000

    Changed the server url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,10 @@ export const environment = {
   dateFormat: 'DD-MM-YYYY',
   shortDateTimeFormat: 'DD.MM.YY HH:mm',
 
-  apiUrl: 'https://49.187.125.194:4000',
-  baseUrl:'https://49.187.125.194:4000',
+  //apiUrl: 'https://49.187.125.194:4000',
+  apiUrl: 'testschedulemebackend.azurewebsites.net',
+
+  //baseUrl:'https://49.187.125.194:4000',
+  baseUrl:'testschedulemebackend.azurewebsites.net',
 
 };
