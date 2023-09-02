0b691cd1551da67a7928adf07f322fc9c88a2e1a
rejkid <rejkid@gmail.com>
2023-09-02 23:22:33 +1000

    Fixed apiUrl and baseUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,13 +10,13 @@ export const environment = {
   
 
   //apiUrl: 'https://49.176.185.39:4000'
-  apiUrl: 'https://oloaschedulemebackend.azurewebsites.net/',
+  apiUrl: 'https://testschedulemebackend.azurewebsites.net:4000',
   //apiUrl: 'http://localhost:4000',
   //apiUrl: 'http://rejkid.hopto.org:4000'
   //apiUrl: 'https://rejkid.hopto.org:4000',
 
   //baseUrl: 'https://rejkid.hopto.org:4000',
-  baseUrl: 'https://oloaschedulemebackend.azurewebsites.net/'
+  baseUrl:'https://testschedulemebackend.azurewebsites.net:4000',
   //baseUrl: 'http://localhost:4000'
 };
 
